8948e12e086cbd925ec62e4b53fafcdc72f3d2b3
nguyenle <lenn@falcongames.com>
2025-02-14 14:36:11 +0700

    init project for logging change data in master db with Debezium

---
+++ spring-boot-cdc-debezium/src/main/java/com/myproject/springbootcdcdebezium/SpringBootCdcDebeziumApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.springbootcdcdebezium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCdcDebeziumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCdcDebeziumApplication.class, args);
+	}
+
+}
---
+++ spring-boot-cdc-debezium/src/test/java/com/myproject/springbootcdcdebezium/SpringBootCdcDebeziumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.springbootcdcdebezium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCdcDebeziumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
