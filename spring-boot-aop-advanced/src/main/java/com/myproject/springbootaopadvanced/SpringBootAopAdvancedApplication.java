dc5105d3ed7c6bd0401c46d119a551df25dbe57e
nguyenle <lenn@falcongames.com>
2025-02-18 17:58:40 +0700

    init project for advanced usecase of aspect oriented programming(Spring AOP)  in spring boot

---
+++ spring-boot-aop-advanced/src/main/java/com/myproject/springbootaopadvanced/SpringBootAopAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.springbootaopadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAopAdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAopAdvancedApplication.class, args);
+	}
+
+}
---
+++ spring-boot-aop-advanced/src/test/java/com/myproject/springbootaopadvanced/SpringBootAopAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.springbootaopadvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAopAdvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
