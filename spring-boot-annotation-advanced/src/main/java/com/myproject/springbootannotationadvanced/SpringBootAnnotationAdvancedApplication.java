aa900fae89655e624ad04767ee637ca210aa6b6e
nguyenle <lenn@falcongames.com>
2025-02-18 17:57:37 +0700

    init project for advanced annotation processing in spring boot

---
+++ spring-boot-annotation-advanced/src/main/java/com/myproject/springbootannotationadvanced/SpringBootAnnotationAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.springbootannotationadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAnnotationAdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAnnotationAdvancedApplication.class, args);
+	}
+
+}
---
+++ spring-boot-annotation-advanced/src/test/java/com/myproject/springbootannotationadvanced/SpringBootAnnotationAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.springbootannotationadvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAnnotationAdvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
