caff5d609f5c00d9d5b5a6e1d150186ab48f8cd0
Le Nguyen Ngoc <lebkp04@gmail.com>
2025-02-20 08:31:26 +0700

    feat(spring-boot-annotation-advanced): preprocessor for XSlf4j annotation

--- spring-boot-annotation-advanced/src/main/java/com/myproject/springbootannotationadvanced/lombok/annotation/XSlf4j.java
+++ spring-boot-annotation-advanced/src/main/java/com/myproject/springbootannotationadvanced/lombok/annotation/XSlf4j.java
@@ -1,9 +1,16 @@
 package com.myproject.springbootannotationadvanced.lombok.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @author nguyenle
  * @since 4:43 PM Wed 2/19/2025
  */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.CLASS)
 public @interface XSlf4j {
-
+    String topic() default "";
 }
--- spring-boot-annotation-advanced/src/main/java/com/myproject/springbootannotationadvanced/lombok/preprocessor/XSlf4jPreprocessor.java
+++ spring-boot-annotation-advanced/src/main/java/com/myproject/springbootannotationadvanced/lombok/preprocessor/XSlf4jPreprocessor.java
@@ -1,14 +1,17 @@
 package com.myproject.springbootannotationadvanced.lombok.preprocessor;
 
 import com.google.auto.service.AutoService;
-import java.util.Set;
-import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.Processor;
-import javax.annotation.processing.RoundEnvironment;
-import javax.annotation.processing.SupportedAnnotationTypes;
-import javax.annotation.processing.SupportedSourceVersion;
+import com.myproject.springbootannotationadvanced.lombok.annotation.XSlf4j;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtField;
+
+import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
+import java.util.Set;
 
 /**
  * @author nguyenle
@@ -19,9 +22,45 @@ import javax.lang.model.element.TypeElement;
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 public class XSlf4jPreprocessor extends AbstractProcessor {
 
-	@Override
-	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-		return false;
-	}
+    private static final String LOGGER_INJECT_PATTERN = """
+            private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger("%s");
+            """;
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        for (Element element : roundEnv.getElementsAnnotatedWith(XSlf4j.class)) {
+            if (element.getKind() == ElementKind.CLASS) {
+                modifyClas((TypeElement) element);
+            }
+        }
+        return true;
+    }
+
+    private void modifyClas(TypeElement classElement) {
+        String className = classElement.getQualifiedName().toString();
+        XSlf4j annotation = classElement.getAnnotation(XSlf4j.class);
+        String topic = annotation.topic().isEmpty() ? className : annotation.topic();
+
+        try {
+            ClassPool classPool = ClassPool.getDefault();
+            CtClass ctClass = classPool.getCtClass(className);
+
+            CtField[] ctDeclaredFields = ctClass.getDeclaredFields();
+            for (CtField field : ctDeclaredFields) {
+                if (field.getType().getName().equals("org.slf4j.Logger")) {
+                    return;
+                }
+            }
+
+            CtField ctField = CtField.make(String.format(LOGGER_INJECT_PATTERN, topic), ctClass);
+            ctClass.addField(ctField);
+
+            ctClass.writeFile();
+            ctClass.detach();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
 
 }
