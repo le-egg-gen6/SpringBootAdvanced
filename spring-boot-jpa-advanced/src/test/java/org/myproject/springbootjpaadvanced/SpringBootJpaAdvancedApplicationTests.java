0c27b3728b81cd04c94245c7b726fdd1e3f8dafd
Le Nguyen Ngoc <lebkp04@gmail.com>
2025-02-22 20:42:06 +0700

    init project for advanced usecase of spring data jpa with hibernate

---
+++ spring-boot-jpa-advanced/src/test/java/org/myproject/springbootjpaadvanced/SpringBootJpaAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myproject.springbootjpaadvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaAdvancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
