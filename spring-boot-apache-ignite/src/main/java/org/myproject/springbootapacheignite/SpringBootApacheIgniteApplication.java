5603330ae91fae557d6516f0b0668e638ac6cd33
Le Nguyen Ngoc <lebkp04@gmail.com>
2025-03-04 01:53:52 +0700

    init project for intergration between spring-boot and apache ignite

---
+++ spring-boot-apache-ignite/src/main/java/org/myproject/springbootapacheignite/SpringBootApacheIgniteApplication.java
@@ -0,0 +1,13 @@
+package org.myproject.springbootapacheignite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApacheIgniteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApacheIgniteApplication.class, args);
+    }
+
+}
---
+++ spring-boot-apache-ignite/src/test/java/org/myproject/springbootapacheignite/SpringBootApacheIgniteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myproject.springbootapacheignite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApacheIgniteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
