e4add2592f38905d3e8a2b274c2093a7d547d658
Le Nguyen Ngoc <lebkp04@gmail.com>
2025-03-04 01:48:04 +0700

    init project for intergration between spring-boot and hazelcast

---
+++ spring-boot-hazelcast/src/main/java/org/myproject/springboothazelcast/SpringBootHazelcastApplication.java
@@ -0,0 +1,13 @@
+package org.myproject.springboothazelcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHazelcastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHazelcastApplication.class, args);
+    }
+
+}
