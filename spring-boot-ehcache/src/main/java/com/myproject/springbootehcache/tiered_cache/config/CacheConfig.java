5a9fa783c88a783c506bc46bfc3d97146dfd2b1b
nguyenle <lenn@falcongames.com>
2025-03-03 16:50:00 +0700

    fix(spring-boot-ehcache): add persistence storage dir into cache config

--- spring-boot-ehcache/src/main/java/com/myproject/springbootehcache/tiered_cache/config/CacheConfig.java
+++ spring-boot-ehcache/src/main/java/com/myproject/springbootehcache/tiered_cache/config/CacheConfig.java
@@ -1,7 +1,10 @@
 package com.myproject.springbootehcache.tiered_cache.config;
 
+import java.io.File;
 import org.ehcache.CacheManager;
 import org.ehcache.config.builders.CacheManagerBuilder;
+import org.ehcache.impl.config.persistence.CacheManagerPersistenceConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,9 +17,15 @@ import org.springframework.context.annotation.Configuration;
 @EnableCaching
 public class CacheConfig {
 
+	@Value("${cache.persist-dir}")
+	private String cachePersistDir;
+
 	@Bean
 	public CacheManager cacheManager() {
-		return CacheManagerBuilder.newCacheManagerBuilder().build(true);
+		File storageDir = new File(cachePersistDir);
+		return CacheManagerBuilder.newCacheManagerBuilder()
+			.with(new CacheManagerPersistenceConfiguration(storageDir))
+			.build(true);
 	}
 
 }
