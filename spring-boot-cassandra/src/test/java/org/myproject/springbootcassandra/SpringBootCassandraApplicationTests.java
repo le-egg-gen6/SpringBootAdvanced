28d609381ea64b7b4e3a37d72e81c6660c63f55a
Le Nguyen Ngoc <lebkp04@gmail.com>
2025-03-04 02:01:46 +0700

    init project for intergration between spring-boot and apache cassandra

---
+++ spring-boot-cassandra/src/test/java/org/myproject/springbootcassandra/SpringBootCassandraApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myproject.springbootcassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCassandraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
