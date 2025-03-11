3829f7d93e2f69da0a36c362107ddbdc4409fdb6
Le Nguyen Ngoc <lebkp04@gmail.com>
2025-03-11 08:39:36 +0700

    init project for integration between spring boot and momo payment service

---
+++ spring-boot-momo/src/main/java/org/myproject/springbootmomo/SpringBootMomoApplication.java
@@ -0,0 +1,13 @@
+package org.myproject.springbootmomo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMomoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMomoApplication.class, args);
+    }
+
+}
---
+++ spring-boot-momo/src/test/java/org/myproject/springbootmomo/SpringBootMomoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myproject.springbootmomo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMomoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
