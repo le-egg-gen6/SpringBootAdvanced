d970bc1a3e09746e4da73651428bc5dde7af5237
nguyenle <lenn@falcongames.com>
2025-03-11 18:23:27 +0700

    init project for integration with 3rd service zalopay

---
+++ spring-boot-zalopay/src/main/java/com/myproject/springbootzalopay/SpringBootZalopayApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.springbootzalopay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootZalopayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootZalopayApplication.class, args);
+	}
+
+}
---
+++ spring-boot-zalopay/src/test/java/com/myproject/springbootzalopay/SpringBootZalopayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.springbootzalopay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootZalopayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
