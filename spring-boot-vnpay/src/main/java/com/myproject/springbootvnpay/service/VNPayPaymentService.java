52cc3e502f89d3715b641f41eee6f3c26fc5b214
nguyenle <lenn@falcongames.com>
2025-03-11 18:18:26 +0700

    feat(spring-boot-vnpay): create method for init pay request

--- spring-boot-vnpay/src/main/java/com/myproject/springbootvnpay/service/VNPayPaymentService.java
+++ spring-boot-vnpay/src/main/java/com/myproject/springbootvnpay/service/VNPayPaymentService.java
@@ -2,18 +2,28 @@ package com.myproject.springbootvnpay.service;
 
 import com.myproject.springbootvnpay.dto.request.InitPaymentRequest;
 import com.myproject.springbootvnpay.dto.response.InitPaymentResponse;
+import com.myproject.springbootvnpay.shared.constant.StringConstant;
 import com.myproject.springbootvnpay.shared.constant.VNPAYCommandConstant;
 import com.myproject.springbootvnpay.shared.constant.VNPAYCoreConstant;
+import com.myproject.springbootvnpay.shared.constant.VNPAYCurrencyConstant;
+import com.myproject.springbootvnpay.shared.constant.VNPAYGoodsTypeConstant;
+import com.myproject.springbootvnpay.shared.constant.VNPAYLocalConstant;
 import com.myproject.springbootvnpay.shared.constant.VNPAYParams;
 import com.myproject.springbootvnpay.utils.DateUtils;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
+import org.springframework.util.StringUtils;
 
 /**
  * @author nguyenle
@@ -23,48 +33,99 @@ import java.util.concurrent.TimeUnit;
 @RequiredArgsConstructor
 public class VNPayPaymentService {
 
-    private final VNPayCryptoService cryptoService;
+	private final VNPayCryptoService cryptoService;
 
-    @Value("${payment.vnpay.init-payment-url}")
-    private String initPaymentPrefixUrl;
+	@Value("${payment.vnpay.tmn-code}")
+	private String tmnCode;
 
-    @Value("${payment.vnpay.return-url}")
-    private String returnUrlFormat;
+	@Value("${payment.vnpay.init-payment-url}")
+	private String initPaymentPrefixUrl;
 
-    @Value("${payment.vnpay.timeout}")
-    private Integer paymentTimeout;
+	@Value("${payment.vnpay.return-url}")
+	private String returnUrlFormat;
 
-    public InitPaymentResponse initPayment(InitPaymentRequest initPaymentRequest) {
-        long amount = initPaymentRequest.getAmount() * VNPAYCoreConstant.DEFAULT_MULTIPLIER;
-        String txnRef = initPaymentRequest.getTxnRef();
-        Long userId = initPaymentRequest.getUserId();
-        String ipAddress = initPaymentRequest.getIpAddress();
-        String requestId = initPaymentRequest.getRequestId();
+	@Value("${payment.vnpay.timeout}")
+	private Integer paymentTimeout;
 
-        Date createdDate = DateUtils.getVNTime();
-        Date expiredDate = DateUtils.getVNTimeAfter(TimeUnit.MINUTES, paymentTimeout);
+	public InitPaymentResponse initPayment(InitPaymentRequest initPaymentRequest) {
+		long amount = initPaymentRequest.getAmount() * VNPAYCoreConstant.DEFAULT_MULTIPLIER;
+		String txnRef = initPaymentRequest.getTxnRef();
+		Long userId = initPaymentRequest.getUserId();
+		String ipAddress = initPaymentRequest.getIpAddress();
+		String requestId = initPaymentRequest.getRequestId();
 
-        String createdDateStr = DateUtils.parseVNPAYFormat(createdDate);
-        String expiredDateStr = DateUtils.parseVNPAYFormat(expiredDate);
+		Date createdDate = DateUtils.getVNTime();
+		Date expiredDate = DateUtils.getVNTimeAfter(TimeUnit.MINUTES, paymentTimeout);
 
-        String orderInfo = buildPaymentDetails(initPaymentRequest);
-        String returnUrl = buildReturnUrl(txnRef);
+		String createdDateStr = DateUtils.parseVNPAYFormat(createdDate);
+		String expiredDateStr = DateUtils.parseVNPAYFormat(expiredDate);
 
-        Map<String, String> params = new HashMap<>();
-        params.put(VNPAYParams.VERSION, VNPAYCoreConstant.SERVICE_VERSION);
-        params.put(VNPAYParams.COMMAND, VNPAYCommandConstant.PAY);
-        params.put(VNPAYParams.AMOUNT, String.valueOf(amount));
-        params.put(VNPAYParams.TXN_REF, txnRef);
+		String orderInfo = buildPaymentDetails(initPaymentRequest);
+		String returnUrl = buildReturnUrl(txnRef);
 
+		Map<String, String> params = new HashMap<>();
+		params.put(VNPAYParams.VERSION, VNPAYCoreConstant.SERVICE_VERSION);
+		params.put(VNPAYParams.COMMAND, VNPAYCommandConstant.PAY);
 
-    }
+		params.put(VNPAYParams.TMN_CODE, tmnCode);
+		params.put(VNPAYParams.AMOUNT, String.valueOf(amount));
+		params.put(VNPAYParams.CURRENCY, VNPAYCurrencyConstant.VND);
 
-    private String buildReturnUrl(String txnRef) {
-        return String.format(returnUrlFormat, txnRef);
-    }
+		params.put(VNPAYParams.TXN_REF, txnRef);
+		params.put(VNPAYParams.RETURN_URL, returnUrl);
 
-    private String buildPaymentDetails(InitPaymentRequest initPaymentRequest) {
-        return String.format("Test %s", initPaymentRequest.getTxnRef());
-    }
+		params.put(VNPAYParams.CREATED_DATE, createdDateStr);
+		params.put(VNPAYParams.EXPIRE_DATE, expiredDateStr);
+
+		params.put(VNPAYParams.IP_ADDRESS, ipAddress);
+		params.put(VNPAYParams.LOCALE, VNPAYLocalConstant.VIETNAM);
+
+		params.put(VNPAYParams.ORDER_INFO, orderInfo);
+		params.put(VNPAYParams.ORDER_TYPE, VNPAYGoodsTypeConstant.BILL_PAYMENT);
+
+		String initPaymentUrl = buildInitPaymentUrl(params);
+		return InitPaymentResponse.builder().vnpUrl(initPaymentUrl).build();
+	}
+
+	private String buildInitPaymentUrl(Map<String, String> params) {
+		StringBuilder hashPayload = new StringBuilder();
+		StringBuilder query = new StringBuilder();
+		List<String> queryField = new ArrayList<>(params.keySet());
+		Collections.sort(queryField);
+
+		Iterator<String> iterator = queryField.iterator();
+		while (iterator.hasNext()) {
+			String key = iterator.next();
+			String value = params.get(key);
+			if (StringUtils.hasText(value)) {
+				hashPayload.append(key);
+				hashPayload.append(StringConstant.EQUALS);
+				hashPayload.append(URLEncoder.encode(value, StandardCharsets.US_ASCII));
+
+				query.append(URLEncoder.encode(key, StandardCharsets.US_ASCII));
+				query.append(StringConstant.EQUALS);
+				query.append(URLEncoder.encode(value, StandardCharsets.US_ASCII));
+
+				if (iterator.hasNext()) {
+					hashPayload.append(StringConstant.AND);
+					query.append(StringConstant.AND);
+				}
+			}
+		}
+		String secureHash = cryptoService.sign(hashPayload.toString());
+		query.append(StringConstant.AND);
+		query.append(VNPAYParams.SECURE_HASH);
+		query.append(StringConstant.EQUALS);
+		query.append(secureHash);
+		return String.format("%s?%s", initPaymentPrefixUrl, query);
+	}
+
+	private String buildReturnUrl(String txnRef) {
+		return String.format(returnUrlFormat, txnRef);
+	}
+
+	private String buildPaymentDetails(InitPaymentRequest initPaymentRequest) {
+		return String.format("Test %s", initPaymentRequest.getTxnRef());
+	}
 
 }
---
+++ spring-boot-vnpay/src/main/java/com/myproject/springbootvnpay/shared/constant/VNPAYGoodsTypeConstant.java
@@ -0,0 +1,128 @@
+package com.myproject.springbootvnpay.shared.constant;
+
+/**
+ * @author nguyenle
+ * @since 6:00 PM Tue 3/11/2025
+ */
+public class VNPAYGoodsTypeConstant {
+	// Food & Consumables
+	public static final String FOOD_CONSUMABLES = "100000";
+	public static final String SNACKS_BEVERAGES = "100001";
+	public static final String DRY_FOOD = "100003";
+	public static final String DAIRY_PRODUCTS = "100004";
+	public static final String CLEANING_SUPPLIES = "100005";
+
+	// Mobile Devices
+	public static final String MOBILE_DEVICES = "110000";
+	public static final String MOBILE_PHONES = "110001";
+	public static final String TABLETS = "110002";
+	public static final String SMART_WATCHES = "110003";
+	public static final String ACCESSORIES = "110004";
+	public static final String SIM_CARDS = "110005";
+
+	// Home Appliances
+	public static final String HOME_APPLIANCES = "120000";
+	public static final String KITCHEN_APPLIANCES = "120001";
+	public static final String HOUSEHOLD_APPLIANCES = "120002";
+	public static final String LARGE_APPLIANCES = "120003";
+
+	// Computers & Office Equipment
+	public static final String COMPUTERS_OFFICE = "130000";
+	public static final String LAPTOPS = "130001";
+	public static final String DESKTOPS = "130002";
+	public static final String MONITORS = "130003";
+	public static final String NETWORK_DEVICES = "130004";
+	public static final String SOFTWARE = "130005";
+	public static final String COMPUTER_ACCESSORIES = "130006";
+	public static final String PRINTERS = "130007";
+	public static final String OTHER_OFFICE_DEVICES = "130008";
+
+	// Electronics & Audio
+	public static final String ELECTRONICS_AUDIO = "140000";
+	public static final String TVS = "140001";
+	public static final String SPEAKERS = "140002";
+	public static final String SOUND_SYSTEMS = "140003";
+	public static final String TECH_TOYS = "140004";
+	public static final String DIGITAL_DEVICES = "140005";
+
+	// Books & Stationery
+	public static final String BOOKS_MAGAZINES = "150000";
+	public static final String STATIONERY = "150001";
+	public static final String GIFTS = "150002";
+	public static final String MUSICAL_INSTRUMENTS = "150003";
+
+	// Sports & Outdoors
+	public static final String SPORTS_OUTDOOR = "160000";
+	public static final String SPORTS_CLOTHING = "160001";
+	public static final String SPORTS_ACCESSORIES = "160002";
+	public static final String YOGA_FITNESS = "160003";
+	public static final String OUTDOOR_EQUIPMENT = "160004";
+
+	// Hotels & Travel
+	public static final String HOTEL_TOURISM = "170000";
+	public static final String DOMESTIC_TOURS = "170001";
+	public static final String INTERNATIONAL_TOURS = "170002";
+	public static final String HOTEL_BOOKING = "170003";
+
+	// Culinary
+	public static final String CULINARY = "180000";
+
+	// Entertainment & Education
+	public static final String ENTERTAINMENT_EDUCATION = "190000";
+	public static final String MOVIE_TICKETS = "190001";
+	public static final String ONLINE_COURSES = "190002";
+	public static final String OTHER_ENTERTAINMENT = "190003";
+	public static final String MEMBERSHIP_CARDS = "190004";
+
+	// Fashion
+	public static final String FASHION = "200000";
+	public static final String WOMEN_FASHION = "200001";
+	public static final String WOMEN_ACCESSORIES = "200002";
+	public static final String MEN_FASHION = "200003";
+	public static final String KIDS_FASHION = "200004";
+
+	// Health & Beauty
+	public static final String HEALTH_BEAUTY = "210000";
+	public static final String SUNSCREEN = "210001";
+	public static final String SKINCARE = "210002";
+	public static final String MAKEUP = "210003";
+	public static final String PERSONAL_CARE = "210004";
+
+	// Mom & Baby
+	public static final String MOM_BABY = "220000";
+	public static final String BABY_MILK = "220001";
+	public static final String BABY_HYGIENE = "220002";
+	public static final String BABY_TOYS = "220003";
+	public static final String BABY_FEEDING = "220004";
+
+	// Kitchenware & Furniture
+	public static final String KITCHENWARE = "230000";
+	public static final String FURNITURE = "230001";
+
+	// Vehicles
+	public static final String VEHICLES = "240000";
+	public static final String MOTORBIKES = "240001";
+	public static final String MOTORBIKE_ACCESSORIES = "240002";
+	public static final String CAR_ACCESSORIES = "240003";
+	public static final String ELECTRIC_BICYCLES = "240004";
+
+	// Bill Payment
+	public static final String BILL_PAYMENT = "250000";
+	public static final String ELECTRICITY_BILL = "250001";
+	public static final String WATER_BILL = "250002";
+	public static final String PHONE_BILL = "250003";
+	public static final String INTERNET_BILL = "250004";
+	public static final String CABLE_TV_BILL = "250005";
+	public static final String SERVICE_BILL = "250006";
+	public static final String AIR_TICKETS = "250007";
+
+	// Buy Vouchers
+	public static final String BUY_VOUCHERS = "260000";
+	public static final String PHONE_CARDS = "260001";
+	public static final String GAME_CARDS = "260002";
+
+	// Pharmacy & Healthcare
+	public static final String PHARMACY_HEALTHCARE = "270000";
+	public static final String MEDICAL_REGISTRATION = "270001";
+}
+
