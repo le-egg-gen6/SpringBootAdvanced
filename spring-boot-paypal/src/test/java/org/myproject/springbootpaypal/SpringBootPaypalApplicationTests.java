473259ba4bef8f4dd730723b0e111aeb9b6733ad
Le Nguyen Ngoc <lebkp04@gmail.com>
2025-03-11 08:40:20 +0700

    init project for integration between spring boot and paypal payment service

---
+++ spring-boot-paypal/src/test/java/org/myproject/springbootpaypal/SpringBootPaypalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myproject.springbootpaypal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPaypalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
