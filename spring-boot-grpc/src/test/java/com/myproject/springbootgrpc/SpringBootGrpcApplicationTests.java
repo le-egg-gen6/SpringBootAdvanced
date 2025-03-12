523298b24800ef8e8e99eb9c3a4af9a78de40ae9
nguyenle <lenn@falcongames.com>
2025-03-12 17:53:24 +0700

    init project try to use grpc instead of restful

---
+++ spring-boot-grpc/src/test/java/com/myproject/springbootgrpc/SpringBootGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.springbootgrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
