16f852636be9b3704a4c0758c6daa2cabf9c2094
nguyenle <lenn@falcongames.com>
2025-04-22 14:47:52 +0700

    init project for centralized payment service use both vnpay, zalopay, momo, paypal

---
+++ spring-boot-payment/src/test/java/com/myproject/springbootpayment/SpringBootPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.springbootpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
